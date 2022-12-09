8dd95ec9e952aecec21c4ac4d4bc2f034434f789
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2022-12-09 14:11:25 -0500

    created document outline

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,32 @@ export default function index() {
   return (
     <>
       
-      <main>index</main>
+      <main>
+        <section className="s">
+          <h1 className="">Headline 1</h1>
+          <p className=""></p>
+        </section>
+        <section className="">
+          <h2 className="">Headline 2</h2>
+          <p className=""></p>
+        </section>
+        <section className="">
+          <h3 className="">Headline 3</h3>
+          <p className=""></p>
+        </section>
+        <section className="">
+          <h4 className="">Headline 4</h4>
+          <p className=""></p>
+        </section>
+        <section className="">
+          <h5 className="">Headline 5</h5>
+          <p className=""></p>
+        </section>
+        <section className="">
+          <h6 className="">Headline 6</h6>
+          <p className=""></p>
+        </section>
+      </main>
     </>
   );
 }
