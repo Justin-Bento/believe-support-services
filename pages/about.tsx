5b287fb59df8be385492857d8a5007c3640cc95f
Justin Bento <bento.a.justin@gmail.com>
2022-12-10 23:41:15 -0500

    created about page

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import React from "react";
+
+export default function about() {
+  return (
+    <>
+      <Head>
+        <title>About - Believe Support Services</title>
+      </Head>
+      <main className="">
+        <div>about</div>
+      </main>
+    </>
+  );
+}
