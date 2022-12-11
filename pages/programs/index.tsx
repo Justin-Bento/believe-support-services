4a65c54096cb02f7c7d87ced2643bca83f007e32
Justin Bento <bento.a.justin@gmail.com>
2022-12-11 11:25:25 -0500

    removed dummy data with sanity infromation

--- pages/programs/index.tsx
+++ pages/programs/index.tsx
@@ -2,15 +2,13 @@ import Head from "next/head";
 import Link from "next/link";
 import React from "react";
 import { sanityClient } from "../../sanity";
+import { Program } from "../../typings";
 
-export default function ProgramHome() {
-  const programName = [
-    { id: 1, Headline: "Day Program" },
-    { id: 2, Headline: "Fitness & Health Program" },
-    { id: 3, Headline: "Residential Program" },
-    { id: 4, Headline: "Summer Camp" },
-    { id: 5, Headline: "Program" },
-  ];
+interface Props {
+  programs: [Program];
+}
+
+export default function ProgramHome({ programs }: Props) {
   return (
     <>
       <Head>
@@ -27,23 +25,20 @@ export default function ProgramHome() {
           </p>
         </section>
         <section className="flex flex-col gap-8 mt-8">
-          {programName.map((context: any) => {
+          {programs.map((context) => {
             return (
               <Link
-                key={context.id}
-                href={`/programs/${context.id}`}
+                key={context._id}
+                href={`/programs/${context.slug.current}`}
                 className=""
               >
                 <article className="relative w-full h-64 transition-all bg-gray-900 rounded-lg hover:opacity-90">
                   <div className="absolute bottom-0 left-0 p-6 text-white">
                     <h2 className="text-lg font-semibold leading-7 tracking-wide text-gray-100">
-                      {context.Headline || "Program Name Goes Here"}
+                      {context.title || "Program Name Goes Here"}
                     </h2>
                     <p className="max-w-4xl mt-1.5 text-base leading-6 tracking-[0.022rem] text-gray-200">
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                      Doloribus dicta quaerat eos sunt at? Tenetur, cum eligendi
-                      voluptates enim odit inventore adipisci at eos, commodi
-                      accusantium delectus, rerum ut officiis!
+                      {context.description || "Program Name Goes Here"}
                     </p>
                   </div>
                 </article>
@@ -57,11 +52,11 @@ export default function ProgramHome() {
 }
 
 export async function getServerSideProps() {
-  const query = `*[_type == "post"]{ _id, title, author -> { name, image }, description, mainImage, slug }`;
-  const posts = await sanityClient.fetch(query)
+  const query = `*[_type == "programs"]{_id, title, description, slug}`;
+  const programs = await sanityClient.fetch(query);
   return {
     props: {
-      posts
+      programs,
     }, // will be passed to the page component as props
-  }
+  };
 }
