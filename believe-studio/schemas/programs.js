1269b1606f8b0ae648de67b8a06031a360bd2467
Justin Bento <bento.a.justin@gmail.com>
2022-12-11 11:24:20 -0500

    created a scema for programs

---
+++ believe-studio/schemas/programs.js
@@ -0,0 +1,39 @@
+export default {
+  name: 'programs',
+  title: 'Programs',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    },
+  ],
+}
