97e053e6d7ffca9a608f1123b7a2404e90a31168
Justin Bento <bento.a.justin@gmail.com>
2022-12-11 00:42:18 -0500

    reduced navbar links

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { HiMenu, HiChevronDown } from 'react-icons/hi';
+import { HiMenu, HiChevronDown } from "react-icons/hi";
 
 export default function Appbar() {
   return (
@@ -10,36 +10,29 @@ export default function Appbar() {
           <Image src="/brand/logo.png" alt="believe support services company logo" width={200} height={200} />
         </Link>
         <nav className="items-center hidden space-x-2 text-sm font-medium text-gray-800 md:flex">
-          <Link
-            href="/programs"
-            className="inline-flex items-center gap-2 px-3 py-2 transition bg-white rounded hover:bg-gray-100"
-          >
-            Programs
-            <HiChevronDown />
-          </Link>
-          <Link
-            href="/about"
-            className="px-3 py-2 transition bg-white rounded hover:bg-gray-100"
-          >
-            About
-          </Link>
-          <Link
-            href="/contact"
-            className="px-3 py-2 transition bg-white rounded hover:bg-gray-100"
-          >
-            Contact
-          </Link>
-          <Link
-            href="/blog"
-            className="px-3 py-2 transition bg-white rounded hover:bg-gray-100"
-          >
-            Blog
-          </Link>
+          <NavLinks Path="/" Label="Home" />
+          <NavLinks Path="/about" Label="About" />
+          <NavLinks Path="/programs" Label="Programs" />
+          <NavLinks Path="/contact" Label="Contact" />
+          <NavLinks Path="/blog" Label="Blog" />
         </nav>
         <button className="flex p-1 text-gray-500 appearance-none md:hidden">
-        <HiMenu size="1.6rem" />
+          <HiMenu size="1.6rem" />
         </button>
       </div>
     </header>
   );
 }
+
+interface NavLink {
+  Path: string;
+  Label: string;
+}
+
+const NavLinks = (props: NavLink) => {
+  return (
+    <Link href={props.Path} className="px-3 py-2 transition bg-white rounded hover:bg-gray-100">
+      {props.Label}
+    </Link>
+  );
+};
