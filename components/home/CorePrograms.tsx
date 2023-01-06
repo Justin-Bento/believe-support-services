f8c22c5de50c993534af88067ba57466b79090be
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-01-06 13:32:48 -0400

    created an core programs sectiion

---
+++ components/home/CorePrograms.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function CorePrograms() {
+  return (
+    <section className="p-4 wrapper">
+      <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+        <div className="p-8 ring-1 rounded-t-xl ring-gray-600">
+          <h2 className="text-xl font-semibold leading-tight text-gray-800 capitalize">Program Name Title</h2>
+          <p className="text-base font-normal text-gray-600 mt-1.5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia magni error est!</p>
+        </div>
+        <div className="p-8 ring-1 rounded-t-xl ring-gray-600">
+          <h2 className="text-xl font-semibold leading-tight text-gray-800 capitalize">Program Name Title</h2>
+          <p className="text-base font-normal text-gray-600 mt-1.5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia magni error est!</p>
+        </div>
+        <div className="p-8 ring-1 rounded-t-xl ring-gray-600">
+          <h2 className="text-xl font-semibold leading-tight text-gray-800 capitalize">Program Name Title</h2>
+          <p className="text-base font-normal text-gray-600 mt-1.5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia magni error est!</p>
+        </div>
+      </div>
+    </section>
+  )
+}
