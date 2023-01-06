c1e988d79ce5951b4805d42e0fbe28bc477ca5f9
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-01-06 14:38:23 -0400

    created a privacy page

---
+++ pages/privacy.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function privacy() {
+  return (
+    <main className="p-4 my-20 space-y-20 wrapper">
+      <section className="max-w-3xl">
+        <h1 className="text-4xl font-bold text-gray-700">Privacy Policy for Believe Support Services</h1>
+        <div className="mt-6 space-y-4">
+          <p>At Believe Support Services, accessible from https://www.believesupportservices.com/, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Believe Support Services and how we use it.</p>
+          <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
+          <p>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Believe Support Services. This policy is not applicable to any information collected offline or via channels other than this website. Our Privacy Policy was created with the help of the <a href="https://postsrc.com/tools/privacy-policy-generator">PostSrc Privacy Policy Generator</a>.</p>
+        </div>
+      </section>
+      <section className="max-w-3xl">
+        <h2 className="text-2xl font-bold text-gray-700">Consent</h2>
+        <p>By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>
+      </section>
+    </main>
+  )
+}
