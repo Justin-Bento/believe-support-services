8e33f93315a7070ef1ebbbc0144ce6456be3ca21
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-01-09 11:59:48 -0400

    changed brand colors to tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,30 +14,8 @@ module.exports = {
       colors: ({ colors }) => ({
         gray: colors.neutral,
         // candlelight
-        primary: {
-          '50': '#fefce8',
-          '100': '#fef9c3',
-          '200': '#fff089',
-          '300': '#fee146',
-          '400': '#fbcc13',
-          '500': '#ebb407',
-          '600': '#cb8a03',
-          '700': '#a26206',
-          '800': '#864d0d',
-          '900': '#713f12',
-        },
-        secondary: {
-          '50': '#edf9ff',
-          '100': '#d7f0ff',
-          '200': '#b8e6ff',
-          '300': '#87d8ff',
-          '400': '#4fc0ff',
-          '500': '#26a1ff',
-          '600': '#0f82ff',
-          '700': '#086aed',
-          '800': '#0d4eb0',
-          '900': '#124a96',
-        },
+        primary: colors.yellow,
+        secondary: colors.blue,
       }),
       fontFamily: {
         sans: ['Public Sans', ...defaultTheme.fontFamily.sans],
