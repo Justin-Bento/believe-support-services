f0da9f93a31f8e8d2684b65eb1815772bf4a77ae
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-01-09 12:00:26 -0400

    changed colors

--- components/home/CallToActionSecondary.tsx
+++ components/home/CallToActionSecondary.tsx
@@ -1,16 +1,16 @@
-export default function CallToActionPrimary() {
+export default function CallToActionSecondary() {
   return (
-    <div className="bg-secondary-50">
+    <div className="bg-blue-50">
       <div className="px-6 py-12 mx-auto max-w-7xl lg:flex lg:items-center lg:justify-between lg:py-24 lg:px-8">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 md:text-4xl">
+        <h3 className="text-3xl font-bold tracking-tight text-gray-900 md:text-4xl">
           <span className="block">Ready to dive in?</span>
-          <span className="block text-secondary-600">Start your free trial today.</span>
-        </h2>
+          <span className="block text-blue-600">Start your free trial today.</span>
+        </h3>
         <div className="flex mt-8 lg:mt-0 lg:flex-shrink-0">
           <div className="inline-flex rounded-md shadow">
-            <a
+          <a
               href="#"
-              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white border border-transparent rounded-md bg-secondary-600 hover:bg-secondary-700"
+              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700"
             >
               Get started
             </a>
--- components/home/CallToActionTertiary.tsx
+++ components/home/CallToActionTertiary.tsx
@@ -2,10 +2,10 @@ export default function CallToActionTertiary() {
   return (
     <div className="bg-gray-50">
       <div className="px-6 py-12 mx-auto max-w-7xl lg:flex lg:items-center lg:justify-between lg:py-24 lg:px-8">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 md:text-4xl">
+        <h5 className="text-3xl font-bold tracking-tight text-gray-900 md:text-4xl">
           <span className="block">Ready to dive in?</span>
           <span className="block text-gray-600">Start your free trial today.</span>
-        </h2>
+        </h5>
         <div className="flex mt-8 lg:mt-0 lg:flex-shrink-0">
           <div className="inline-flex rounded-md shadow">
             <a
