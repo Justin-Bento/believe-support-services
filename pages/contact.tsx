27b7289b3acf54507881ab05d5a423f282c6acbc
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-01-10 10:59:38 -0400

    replaced developer api with testimonials

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -110,10 +110,10 @@ export default function contact() {
                 <path d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h12z" />
               </svg>
               <div>
-                <h3 className="font-semibold text-gray-800 dark:text-gray-200">Developer APIs</h3>
-                <p className="mt-1 text-sm text-gray-500">Check out our development quickstart guide.</p>
+                <h3 className="font-semibold text-gray-800 dark:text-gray-200">Testimonials</h3>
+                <p className="mt-1 text-sm text-gray-500">Check out what other people are saying about our program.</p>
                 <a className="inline-flex items-center mt-2 text-sm font-medium text-gray-600 gap-x-2 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">
-                  Contact sales
+                  Visit Testimonials
                   <svg className="w-2.5 h-2.5 transition ease-in-out group-hover:translate-x-1" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M0.975821 6.92249C0.43689 6.92249 -3.50468e-07 7.34222 -3.27835e-07 7.85999C-3.05203e-07 8.37775 0.43689 8.79749 0.975821 8.79749L12.7694 8.79748L7.60447 13.7596C7.22339 14.1257 7.22339 14.7193 7.60447 15.0854C7.98555 15.4515 8.60341 15.4515 8.98449 15.0854L15.6427 8.68862C16.1191 8.23098 16.1191 7.48899 15.6427 7.03134L8.98449 0.634573C8.60341 0.268455 7.98555 0.268456 7.60447 0.634573C7.22339 1.00069 7.22339 1.59428 7.60447 1.9604L12.7694 6.92248L0.975821 6.92249Z" fill="currentColor" />
                   </svg>
