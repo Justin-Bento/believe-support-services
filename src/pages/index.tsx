b80d6129c6212eefb386002ffb8b7fd344328d4f
Justin Bento <bento.a.justin@gmail.com>
2023-04-12 01:36:16 -0400

    created a section for a company newsletter so notify people about any updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,8 @@ export default function index() {
       <Appbar />
       <main className="container p-4 mx-auto space-y-32">
         <Hero />
+
+        <Newsletter />
       </main>
     </>
   );
@@ -134,4 +136,60 @@ function Hero() {
       </section>
     </>
   );
+}
+
+
+function Newsletter() {
+  return (
+    <div className="py-16 bg-primary-50">
+        <div className="relative px-6 py-24 overflow-hidden bg-primary-300 isolate sm:rounded-3xl sm:px-24 xl:py-32">
+          <h2 className="max-w-2xl mx-auto text-3xl font-bold tracking-tight text-center text-primary-950 sm:text-4xl">
+            Get notified when we’re launching.
+          </h2>
+          <p className="max-w-xl mx-auto mt-2 text-lg leading-8 text-center text-primary-950">
+            Reprehenderit ad esse et non officia in nulla. Id proident tempor incididunt nostrud nulla et culpa.
+          </p>
+          <form className="flex max-w-md mx-auto mt-10 gap-x-4">
+            <label htmlFor="email-address" className="sr-only">
+              Email address
+            </label>
+            <input
+              id="email-address"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              className="min-w-0 flex-auto rounded-md border-0 bg-primary-900/10 px-3.5 py-2  placeholder-slate-900 ring-1 ring-inset ring-primary-400/50 focus:ring-2 focus:ring-inset focus:ring-primary-300 sm:text-sm sm:leading-6"
+              placeholder="Enter your email"
+            />
+            <button
+              type="submit"
+              className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            >
+              Notify me
+            </button>
+          </form>
+          <svg
+            viewBox="0 0 1024 1024"
+            className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2"
+            aria-hidden="true"
+          >
+            <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
+            <defs>
+              <radialGradient
+                id="759c1415-0410-454c-8f7c-9a820de03641"
+                cx={0}
+                cy={0}
+                r={1}
+                gradientUnits="userSpaceOnUse"
+                gradientTransform="translate(512 512) rotate(90) scale(512)"
+              >
+                <stop stopColor="#EEBC03" />
+                <stop offset={1} stopColor="#EEBC03" stopOpacity={0} />
+              </radialGradient>
+            </defs>
+          </svg>
+        </div>
+    </div>
+  )
 }
