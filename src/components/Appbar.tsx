94187a0487bd5544e600c82d8a08dc10e2e74362
Justin Bento <bento.a.justin@gmail.com>
2023-04-12 02:26:38 -0400

    fixed darkmode fro appbar

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -3,7 +3,7 @@ import { Dialog } from '@headlessui/react'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 import Link from 'next/link'
 import Logo from "@/components/Logo"
-import {appbar_navigation} from "@/lib/data"
+import { appbar_navigation } from "@/lib/data"
 
 export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
@@ -20,7 +20,7 @@ export default function Example() {
         <div className="flex lg:hidden">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-primary-700 dark:text-primary-50"
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
@@ -37,19 +37,15 @@ export default function Example() {
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full px-6 py-6 overflow-y-auto bg-primary-50 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full px-6 py-6 overflow-y-auto bg-theme sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="w-auto h-8"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
-            </a>
+            <Link href="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">Believe Support Services</span>
+              <Logo fill={`fill-primary-900 dark:fill-primary-50`} />
+            </Link>
             <button
               type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              className="-m-2.5 rounded-md p-2.5 text-primary-700 dark:text-primary-50"
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
@@ -57,7 +53,7 @@ export default function Example() {
             </button>
           </div>
           <div className="flow-root mt-6">
-            <div className="-my-6 divide-y divide-gray-500/10">
+            <div className="-my-6 divide-y divide-primary-500/10 dark:divide-gray-100">
               <div className="py-6 space-y-2">
                 {appbar_navigation.map((item) => (
                   <a
