ccf1ee81ca403e31c8dcdf7bb316b6d2037696be
Justin Bento <bento.a.justin@gmail.com>
2023-04-12 00:16:23 -0400

    setup typescale with brands fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,45 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          50: "#f1f7fe",
+          100: "#e1effd",
+          200: "#bddefa",
+          300: "#82c3f7",
+          400: "#43a6f0",
+          500: "#1789e0",
+          600: "#0a6bbf",
+          700: "#0a569a",
+          800: "#0c4980",
+          900: "#103e6a",
+          950: "#0b2746",
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        sans: ["OpenSans", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    // ...
+  ],
+};
