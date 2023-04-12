48fd3afe16677d15eadec4231c110c5165e21411
Justin Bento <bento.a.justin@gmail.com>
2023-04-12 00:17:15 -0400

    created a hero section for the website

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,128 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineLink } from "react-icons/ai";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default function index() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="container p-4 mx-auto space-y-32">
+      <Hero />
     </main>
-  )
+  );
+}
+
+function Hero() {
+  return (
+    <>
+      <section className="">
+        <div className="my-8 space-y-6">
+          <div className="justify-start hidden sm:flex sm:justify-center">
+            <div className="relative px-3 py-1 rounded-full body-medium text-primary-950 dark:text-primary-50 ring-1 ring-primary-900/10 dark:ring-primary-100/30 hover:ring-primary-900/20 dark:hover:ring-primary-100/30">
+              Upcomming Events At Believe. &nbsp;
+              <a
+                href="#"
+                className="font-semibold text-primary-600 dark:text-primary-300 primary-950 space-nowrap"
+              >
+                <span className="absolute inset-0" aria-hidden="true" />
+                Read more <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+          <h1 className="font-bold display-medium md:text-center">
+            A Strong Community Starts With Beliving!
+          </h1>
+          <p className="text-lg body-large text-start sm:text-center lg:max-w-[70ch] lg:mx-auto">
+            We Help individuals with complex needs, by offering programs focused
+            on life skills, personal development, recreational activities, and
+            social support groups, to help them be the best version on
+            themselves.{" "}
+          </p>
+          <div className="flex flex-col justify-start gap-4 md:flex-row md:justify-center">
+            <Link
+              href="/signup"
+              className="px-3 py-2 text-sm font-semibold rounded-md text-primary-50 bg-primary-600 hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+            >
+              Get In Touch With A Representative
+            </Link>
+            <Link
+              href="/programs"
+              className="rounded-md bg-primary-50 px-2.5 py-1.5 body-medium inline-flex items-center font-semibold text-primary-600 shadow-sm hover:bg-primary-100"
+            >
+              View Available Programs
+            </Link>
+          </div>
+        </div>
+        {/* END: Hero :: Actions */}
+        <div className="overflow-hidden bg-transparent ">
+          <div className="px-4 py-5 sm:p-6 w-full h-[420px] md:h-[640px] relative z-10">
+            <Image
+              fill
+              quality={40}
+              src="/media/hero-image.webp"
+              alt="Hodges Bay Club garnished with our Island Microgreens"
+              className="object-cover object-center rounded-xl"
+            />
+          </div>
+        </div>
+        {/* END: Hero :: Media */}
+        <div className="grid grid-cols-1 gap-8 mt-8 lg:grid-cols-3">
+          {/* END: Hero :: 01 Core Programs */}
+          <div className="overflow-hidden transition-all border rounded-lg bg-primary-200 hover:bg-primary-300/30 hover:cursor-pointer border-primary-300">
+            <div className="px-4 py-5 space-y-1.5 sm:p-6">
+              <h2 className="px-2 font-bold title-medium">Day Programs</h2>
+              <p className="px-2 body-medium">
+                Semi-Independent program designed to cater to adults with
+                complex needs duirng their stay.
+              </p>
+              <button
+                type="button"
+                className="inline-flex items-center gap-1.5 px-2 py-1.5 text-sm font-semibold text-primary-600 "
+              >
+                <AiOutlineLink />
+                Button text
+              </button>
+            </div>
+          </div>
+          {/* END: Hero :: 02 Core Programs */}
+          <div className="overflow-hidden transition-all border rounded-lg bg-primary-200 hover:bg-primary-300/30 hover:cursor-pointer border-primary-300">
+            <div className="px-4 py-5 space-y-1.5 sm:p-6">
+              <h2 className="px-2 font-bold title-medium">
+                Fitness & Health Programs
+              </h2>
+              <p className="px-2 body-medium">
+                Semi-Independent program designed to cater to adults with
+                complex needs duirng their stay.
+              </p>
+              <button
+                type="button"
+                className="inline-flex items-center gap-1.5 px-2 py-1.5 text-sm font-semibold text-primary-600 "
+              >
+                <AiOutlineLink />
+                Button text
+              </button>
+            </div>
+          </div>
+          {/* END: Hero :: 03 Core Programs */}
+          <div className="overflow-hidden transition-all border rounded-lg bg-primary-200 hover:bg-primary-300/30 hover:cursor-pointer border-primary-300">
+            <div className="px-4 py-5 space-y-1.5 sm:p-6">
+              <h2 className="px-2 font-bold title-medium">
+                Residential Programs
+              </h2>
+              <p className="px-2 body-medium">
+                Semi-Independent program designed to cater to adults with
+                complex needs duirng their stay.
+              </p>
+              <button
+                type="button"
+                className="inline-flex items-center gap-1.5 px-2 py-1.5 text-sm font-semibold text-primary-600 "
+              >
+                <AiOutlineLink />
+                Button text
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* END: Hero :: Core Programs */}
+      </section>
+    </>
+  );
 }
