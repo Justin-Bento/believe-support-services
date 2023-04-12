47fe328bcda0197af307abf58c36531a1db3c8d0
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-04-12 14:42:34 -0400

    looped programs to indiviual need

--- src/pages/programs.tsx
+++ src/pages/programs.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Appbar from "@/components/Appbar";
 import Footer from "@/components/Footer";
 import { app_programs } from "@/lib/data";
+import Link from "next/link";
 
 export default function programs() {
   return (
@@ -15,16 +16,20 @@ export default function programs() {
         />
       </Head>
       <Appbar />
-      <div className="container p-4 mx-auto mt-16 mb-32 space-y-16">
-        <h1 className="">Programs To Keep Stay Beliving</h1>
-        {app_programs.map((data:any) => {
-          return (
-            <>
-              <h2 className="headline-msmalledium">{data.title}</h2>
-              <p className="body-large">{data.subtitle}</p>
-            </>
-          )
-        })}
+      <div className="container p-4 mx-auto my-32 mt-16">
+        <h1 className="mb-8 text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-300 sm:text-4xl">Programs To Keep Stay Beliving</h1>
+        <div className="flex flex-col gap-8">
+          {app_programs.map((data: any) => {
+            return (
+              <>
+                <Link href={`/programs/${data.title.toString().toLowerCase().split(' ').join('-')}`} className="flex flex-col gap-2 p-12 rounded-2xl bg-primary-900">
+                  <h2 className="title-medium">{data.title}</h2>
+                  <p className="max-w-2xl prose-sm text-slate-900 dark:text-slate-300">{data.subtitle}</p>
+                </Link>
+              </>
+            )
+          })}
+        </div>
       </div>
       <Footer />
     </>
