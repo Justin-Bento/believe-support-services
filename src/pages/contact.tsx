03d19b2d8ae96684bb32e2feba325f9b7d0d01ca
Justin Bento <bento.a.justin@gmail.com>
2023-04-13 22:42:58 -0400

    created a contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,146 @@
+import Appbar from "@/components/Appbar";
+import Footer from "@/components/Footer";
+import { AiFillHome, AiFillMail, AiFillPhone } from 'react-icons/ai'
+
+export default function Example() {
+  return (
+    <>
+      <Appbar />
+      <div className="relative bg-primary-50 isolate">
+        <div className="container grid grid-cols-1 mx-auto lg:grid-cols-2">
+          <div className="relative px-6 pt-24 pb-20 sm:pt-32 lg:static lg:px-4">
+            <div className="max-w-xl mx-auto lg:mx-0 lg:max-w-lg">
+              <div className="absolute inset-y-0 left-0 w-full overflow-hidden bg-primary-50/10 -z-10 lg:w-1/2">
+              </div>
+              <h2 className="headline-medium">Get in touch</h2>
+              <p className="mt-2 ">
+                Proin volutpat consequat porttitor cras nullam gravida at. Orci molestie a eu arcu. Sed ut tincidunt
+                integer elementum id sem. Arcu sed malesuada et magna.
+              </p>
+              <dl className="mt-10 space-y-4 text-base leading-7 text-gray-600">
+                <div className="flex gap-x-4">
+                  <dt className="flex-none">
+                    <span className="sr-only">Address</span>
+                    <AiFillHome className="w-6 text-gray-400 h-7" aria-hidden="true" />
+                  </dt>
+                  <dd>
+                    545 Mavis Island
+                    <br />
+                    Chicago, IL 99191
+                  </dd>
+                </div>
+                <div className="flex gap-x-4">
+                  <dt className="flex-none">
+                    <span className="sr-only">Telephone</span>
+                    <AiFillPhone className="w-6 text-gray-400 h-7" aria-hidden="true" />
+                  </dt>
+                  <dd>
+                    <a className="hover:text-gray-900" href="tel:+1 (555) 234-5678">
+                      +1 (555) 234-5678
+                    </a>
+                  </dd>
+                </div>
+                <div className="flex gap-x-4">
+                  <dt className="flex-none">
+                    <span className="sr-only">Email</span>
+                    <AiFillMail className="w-6 text-gray-400 h-7" aria-hidden="true" />
+                  </dt>
+                  <dd>
+                    <a className="hover:text-gray-900" href="mailto:hello@example.com">
+                      hello@example.com
+                    </a>
+                  </dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+          <form action="#" method="POST" className="px-6 pt-20 pb-24 sm:pb-32 lg:px-8 lg:py-48">
+            <div className="max-w-xl mx-auto lg:mr-0 lg:max-w-lg">
+              <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                <div>
+                  <label htmlFor="first-name" className="font-semibold capitalize body-medium">
+                    First name
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autoComplete="given-name"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label htmlFor="last-name" className="font-semibold capitalize body-medium">
+                    Last name
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      autoComplete="family-name"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <label htmlFor="email" className="font-semibold capitalize body-medium">
+                    Email
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      autoComplete="email"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <label htmlFor="phone-number" className="font-semibold capitalize body-medium">
+                    Phone number
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="tel"
+                      name="phone-number"
+                      id="phone-number"
+                      autoComplete="tel"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <label htmlFor="message" className="font-semibold capitalize body-medium">
+                    Message
+                  </label>
+                  <div className="mt-2.5">
+                    <textarea
+                      name="message"
+                      id="message"
+                      rows={4}
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                      defaultValue={''}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-end mt-8">
+                <button
+                  type="submit"
+                  className="rounded-md bg-primary-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+                >
+                  Send message
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
