2caa51ae7a2a049fea889ee8c4ee9d7e13f4af6c
Justin Bento <bento.a.justin@gmail.com>
2023-07-20 16:56:17 -0400

    imported website meta data

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Head from 'next/head';
 import Appbar from "@/components/Appbar";
 import Footer from "@/components/Footer";
 
 export default function events() {
   return (
     <>
+      <Head>
+        <title>Events - Believe Support Services</title>
+      </Head>
       <Appbar />
       <main className="h-[100vh]"></main>
       <Footer />
