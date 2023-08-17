3afd2e8c0c53b3e9578639a09428ea31c38a57b6
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-08-17 09:38:07 -0400

    removed calander from events page

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -2,10 +2,8 @@ import React, { useState } from 'react'
 import Head from 'next/head';
 import Appbar from "@/components/Appbar";
 import Footer from "@/components/Footer";
-import { Calendar } from '@/components/Calendar';
 
 export default function events() {
-  const [date, setDate] = useState<Date>(new Date())
   return (
     <>
       <Head>
@@ -16,12 +14,7 @@ export default function events() {
         <h1 className="scroll-m-20 font-extrabold tracking-normal text-4xl dark:text-primary-100">Join Us for Upcoming Events</h1>
         <p className="leading-7 tracking-wider dark:text-primary-100 mt-4">Stay connected with Believe Support Services through our exciting and enriching events. From workshops and seminars to community gatherings and celebrations, our events are designed to promote learning, social interaction, and a sense of belonging.</p>
         <section className="mt-12">
-          <Calendar
-            mode="single"
-            selected={date}
-            onSelect={() => setDate}
-            className="rounded-lg border w-full lg:w-fit p-4"
-          />
+          
         </section>
       </main>
       <Footer />
