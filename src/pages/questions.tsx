a0f74e50c165c295f27d52f6de77942f1b7c7f78
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-08-17 00:03:12 -0400

    updated dark mode support for question theme

--- src/pages/questions.tsx
+++ src/pages/questions.tsx
@@ -12,15 +12,15 @@ export default function questions() {
     </Head>
     <Appbar />
       <main className="p-4 my-32 lg:container lg:mx-auto lg:p-0">
-        <h1 className="headline-large">Questions With Believe Support Services</h1>
-        <p className="mt-1.5 text-xl leading-7 tracking-wide">After sending a survey we pulled some of the most frequently asked questions asked by our customers. We received and compiled them all into one place for your convenience. Have more questions? Please reach out!</p>
+        <h1 className="scroll-m-20 font-extrabold tracking-wide text-5xl dark:text-primary-100">Questions With Believe Support Services</h1>
+        <p className="leading-7 text-xl tracking-wider dark:text-primary-100 mt-6">After sending a survey we pulled some of the most frequently asked questions asked by our customers. We received and compiled them all into one place for your convenience. Have more questions? Please reach out!</p>
         <section className='my-8 space-y-4'>
           {frequently_asked_questions.map((data: any) => {
             return (
               <>
-                <div className="p-4 border rounded">
-                  <h2 className="text-base font-bold opacity-70  capitalize [&[align=center]]:text-center [&[align=right]]:text-right">{data.question}</h2>
-                  <p className="text-sm tracking-wide font-normal leading-7 [&[align=center]]:text-center [&[align=right]]:text-right">{data.awnser}</p>
+                <div className="p-4 bg-theme dark:border-opacity-0 rounded-xl">
+                  <h2 className="font-semibold tracking-wider text-md capitalize dark:text-primary-100">{data.question}</h2>
+                  <p className="leading-7 text-sm tracking-wider dark:text-primary-100/75">{data.awnser}</p>
                 </div>
               </>
             )
