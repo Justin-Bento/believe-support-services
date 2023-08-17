ddc53b85df48fad560a11d12d458ec31bd562cb8
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-08-16 23:03:50 -0400

    fixed typography and colour on CallToAction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -235,20 +235,20 @@ function CallToAction() {
     <div className="bg-theme rounded-2xl">
       <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
         <div className="max-w-2xl mx-auto text-center">
-          <h2 className="font-bold display-small">
-            Boost your productivity.
+          <h2 className="scroll-m-20 pb-2 text-4xl font-bold dark:text-primary-50 capitalize tracking-wide transition-colors first:mt-0">
+            Boost your Knowledege.
             <br />
-            Start using our app today.
+            Start Believing In Yourself Today.
           </h2>
-          <p className="max-w-xl mx-auto mt-6 body-large">
+          <p className="max-w-xl mx-auto mt-4 leading-7 tracking-wide text-opacity-75">
             Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur
             commodo do ea.
           </p>
           <div className="flex items-center justify-center mt-10 gap-x-6">
-            <Button variant="ghost">
+            <Button>
               <Link href="/signup">Get started</Link>
             </Button>
-            <Button variant="outline">
+            <Button variant="ghost">
               <Link href="/signup">Learn more <span aria-hidden="true">→</span></Link>
             </Button>
           </div>
