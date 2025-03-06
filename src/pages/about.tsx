85e8b22b085ffa030ac184d15c7ce27832ba8bd5
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-06 11:23:02 -0500

    Moved mission section to default function to reduce code complexity within the about page.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,64 +8,74 @@ export default function about() {
     <>
       <Head>
         <title>About - Believe Support Services</title>
-        <meta name="description" content="Helping individuals with complex needs to help them be the best version on themselves and their community." />
+        <meta
+          name="description"
+          content="Helping individuals with complex needs to help them be the best version on themselves and their community."
+        />
       </Head>
       <Appbar />
-      <main className='container p-4 mx-auto my-32 space-y-32'>
-        <Mission />
+      <main className="container p-4 mx-auto my-32 space-y-32">
+        <section className="">
+          <div className="max-w-2xl mx-auto lg:mx-0 lg:max-w-none">
+            <h2 className="font-extrabold capitalize display-small">
+              Our mission
+            </h2>
+            <div className="flex flex-col mt-6 gap-x-8 gap-y-20 lg:flex-row">
+              <div className="lg:w-full lg:max-w-2xl lg:flex-auto">
+                <p className="prose-lg dark:text-slate-100">
+                  Aliquet nec orci mattis amet quisque ullamcorper neque, nibh
+                  sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque
+                  id at vitae feugiat egestas ac. Diam nulla orci at in viverra
+                  scelerisque eget. Eleifend egestas fringilla sapien.
+                </p>
+                <div className="max-w-xl mt-10 prose dark:text-slate-300">
+                  <p>
+                    Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
+                    risus enim. Mattis mauris semper sed amet vitae sed turpis
+                    id. Id dolor praesent donec est. Odio penatibus risus
+                    viverra tellus varius sit neque erat velit. Faucibus commodo
+                    massa rhoncus, volutpat. Dignissim sed eget risus enim.
+                    Mattis mauris semper sed amet vitae sed turpis id.
+                  </p>
+                  <p className="mt-10">
+                    Et vitae blandit facilisi magna lacus commodo. Vitae sapien
+                    duis odio id et. Id blandit molestie auctor fermentum
+                    dignissim. Lacus diam tincidunt ac cursus in vel. Mauris
+                    varius vulputate et ultrices hac adipiscing egestas. Iaculis
+                    convallis ac tempor et ut. Ac lorem vel integer orci.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/*
+         * End of Mission Section
+         */}
         <AboutCompanyMedia />
         <Values />
         <Testimonials />
       </main>
       <Footer />
     </>
-  )
-
+  );
 }
 
-function Mission() {
-  return (
-    <>
-      {/* Content section */}
-      <div className="">
-        <div className="max-w-2xl mx-auto lg:mx-0 lg:max-w-none">
-          <h2 className="font-extrabold capitalize display-small">Our mission</h2>
-          <div className="flex flex-col mt-6 gap-x-8 gap-y-20 lg:flex-row">
-            <div className="lg:w-full lg:max-w-2xl lg:flex-auto">
-              <p className="prose-lg dark:text-slate-100">
-                Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam
-                eget aliquam. Quisque id at vitae feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget.
-                Eleifend egestas fringilla sapien.
-              </p>
-              <div className="max-w-xl mt-10 prose dark:text-slate-300">
-                <p>
-                  Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed
-                  amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius
-                  sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim.
-                  Mattis mauris semper sed amet vitae sed turpis id.
-                </p>
-                <p className="mt-10">
-                  Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie
-                  auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et
-                  ultrices hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  )
-}
 function AboutCompanyMedia() {
   return (
     <>
       {/* Image section */}
       <div className="aspect-[5/2] w-full relative">
-        <Image fill quality={50} src="https://images.unsplash.com/photo-1529156069898-49953e39b3ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80" alt="" className="object-cover object-center " />
+        <Image
+          fill
+          quality={50}
+          src="https://images.unsplash.com/photo-1529156069898-49953e39b3ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80"
+          alt=""
+          className="object-cover object-center "
+        />
       </div>
     </>
-  )
+  );
 }
 function Testimonials() {
   return (
@@ -76,19 +86,21 @@ function Testimonials() {
             <figure className="flex flex-col justify-between flex-auto mt-10">
               <blockquote className="prose dark:text-slate-300">
                 <p>
-                  “Amet amet eget scelerisque tellus sit neque faucibus non eleifend. Integer eu praesent at a. Ornare
-                  arcu gravida natoque erat et cursus tortor consequat at. Vulputate gravida sociis enim nullam
-                  ultricies habitant malesuada lorem ac. Tincidunt urna dui pellentesque sagittis.”
+                  “Amet amet eget scelerisque tellus sit neque faucibus non
+                  eleifend. Integer eu praesent at a. Ornare arcu gravida
+                  natoque erat et cursus tortor consequat at. Vulputate gravida
+                  sociis enim nullam ultricies habitant malesuada lorem ac.
+                  Tincidunt urna dui pellentesque sagittis.”
                 </p>
               </blockquote>
               <figcaption className="flex items-center mt-10 gap-x-6">
                 <div className="relative h-14 w-14">
-                <Image
-                  fill
-                  className="rounded-full "
-                  src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
+                  <Image
+                    fill
+                    className="rounded-full "
+                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
                 </div>
                 <div className="body-medium">
                   <div className="font-semibold">Judith Black</div>
@@ -101,19 +113,20 @@ function Testimonials() {
             <figure className="flex flex-col justify-between flex-auto mt-10">
               <blockquote className="prose dark:text-slate-300">
                 <p>
-                  “Excepteur veniam labore ullamco eiusmod. Pariatur consequat proident duis dolore nulla veniam
-                  reprehenderit nisi officia voluptate incididunt exercitation exercitation elit. Nostrud veniam sint
-                  dolor nisi ullamco.”
+                  “Excepteur veniam labore ullamco eiusmod. Pariatur consequat
+                  proident duis dolore nulla veniam reprehenderit nisi officia
+                  voluptate incididunt exercitation exercitation elit. Nostrud
+                  veniam sint dolor nisi ullamco.”
                 </p>
               </blockquote>
               <figcaption className="flex items-center mt-10 gap-x-6">
                 <div className="relative h-14 w-14">
-                <Image
-                  fill
-                  className="rounded-full bg-gray-50"
-                  src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
+                  <Image
+                    fill
+                    className="rounded-full bg-gray-50"
+                    src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
                 </div>
                 <div className="body-medium">
                   <div className="font-semibold ">Joseph Rodriguez</div>
@@ -125,63 +138,68 @@ function Testimonials() {
         </div>
       </div>
     </section>
-  )
+  );
 }
 function Values() {
   return (
     <>
-
       {/* Values section */}
       <div className="">
         <div className="max-w-2xl mx-auto lg:mx-0">
-          <h2 className="font-extrabold capitalize headline-large dark:text-white">Our values</h2>
+          <h2 className="font-extrabold capitalize headline-large dark:text-white">
+            Our values
+          </h2>
           <p className="mt-6 prose dark:text-slate-300">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in
-            accusamus quisquam.
+            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
+            voluptatum cupiditate veritatis in accusamus quisquam.
           </p>
         </div>
         <dl className="grid max-w-2xl grid-cols-1 mx-auto mt-16 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {values.map((value) => (
             <div key={value.name}>
-              <dt className="font-semibold capitalize title-medium dark:text-slate-300">{value.name}</dt>
-              <dd className="mt-1 prose-sm text-slate-600 dark:text-slate-300">{value.description}</dd>
+              <dt className="font-semibold capitalize title-medium dark:text-slate-300">
+                {value.name}
+              </dt>
+              <dd className="mt-1 prose-sm text-slate-600 dark:text-slate-300">
+                {value.description}
+              </dd>
             </div>
           ))}
         </dl>
       </div>
     </>
-  )
+  );
 }
 
 const values = [
   {
-    name: 'Be world-class',
+    name: "Be world-class",
     description:
-      'Aut illo quae. Ut et harum ea animi natus. Culpa maiores et sed sint et magnam exercitationem quia. Ullam voluptas nihil vitae dicta molestiae et. Aliquid velit porro vero.',
+      "Aut illo quae. Ut et harum ea animi natus. Culpa maiores et sed sint et magnam exercitationem quia. Ullam voluptas nihil vitae dicta molestiae et. Aliquid velit porro vero.",
   },
   {
-    name: 'Share everything you know',
+    name: "Share everything you know",
     description:
-      'Mollitia delectus a omnis. Quae velit aliquid. Qui nulla maxime adipisci illo id molestiae. Cumque cum ut minus rerum architecto magnam consequatur. Quia quaerat minima.',
+      "Mollitia delectus a omnis. Quae velit aliquid. Qui nulla maxime adipisci illo id molestiae. Cumque cum ut minus rerum architecto magnam consequatur. Quia quaerat minima.",
   },
   {
-    name: 'Always learning',
+    name: "Always learning",
     description:
-      'Aut repellendus et officiis dolor possimus. Deserunt velit quasi sunt fuga error labore quia ipsum. Commodi autem voluptatem nam. Quos voluptatem totam.',
+      "Aut repellendus et officiis dolor possimus. Deserunt velit quasi sunt fuga error labore quia ipsum. Commodi autem voluptatem nam. Quos voluptatem totam.",
   },
   {
-    name: 'Be supportive',
+    name: "Be supportive",
     description:
-      'Magnam provident veritatis odit. Vitae eligendi repellat non. Eum fugit impedit veritatis ducimus. Non qui aspernatur laudantium modi. Praesentium rerum error deserunt harum.',
+      "Magnam provident veritatis odit. Vitae eligendi repellat non. Eum fugit impedit veritatis ducimus. Non qui aspernatur laudantium modi. Praesentium rerum error deserunt harum.",
   },
   {
-    name: 'Take responsibility',
+    name: "Take responsibility",
     description:
-      'Sit minus expedita quam in ullam molestiae dignissimos in harum. Tenetur dolorem iure. Non nesciunt dolorem veniam necessitatibus laboriosam voluptas perspiciatis error.',
+      "Sit minus expedita quam in ullam molestiae dignissimos in harum. Tenetur dolorem iure. Non nesciunt dolorem veniam necessitatibus laboriosam voluptas perspiciatis error.",
   },
   {
-    name: 'Enjoy downtime',
+    name: "Enjoy downtime",
     description:
-      'Ipsa in earum deserunt aut. Quos minus aut animi et soluta. Ipsum dicta ut quia eius. Possimus reprehenderit iste aspernatur ut est velit consequatur distinctio.',
+      "Ipsa in earum deserunt aut. Quos minus aut animi et soluta. Ipsum dicta ut quia eius. Possimus reprehenderit iste aspernatur ut est velit consequatur distinctio.",
   },
-]
+];
