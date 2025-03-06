316adc60782c7817fc6dd09d60564bf2795a40ea
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-06 12:05:07 -0500

    Removed function to reduce code complexity

--- src/pages/programs.tsx
+++ src/pages/programs.tsx
@@ -22,49 +22,37 @@ export default function programs() {
         <div className="grid sm:grid-cols-2 2xl:grid-cols-3 gap-8 mt-12">
           {programs_info.map((data: any) => {
             return (
-              <ProgramCard
+              <Link
                 key={data.title.toString()}
-                URL={`/programs/${CreateSlug(data.title)}`}
-                Headline={data.title}
-                Supporting={data.description}
-              />
+                href={`/programs/${CreateSlug(data.title)}`}
+                className="hover:opacity-80 active:ring-1 transition-all"
+              >
+                <div className="bg-primary-50 dark:bg-primary-900 min-h-[430px] rounded-xl">
+                  <div className="w-full h-64 relative">
+                    <Image
+                      fill
+                      src="/media/hero-image.webp"
+                      alt={data.title}
+                      className="object-cover rounded-xl hover:grayscale-[10%]"
+                    />
+                  </div>
+                  <div className="p-8">
+                    <h2 className="text-lg font-semibold text-slate-900 dark:text-slate-100">
+                      {data.title}
+                    </h2>
+                    <p className="leading-7 line-clamp-3 tracking-wider text-opacity-75 text-slate-700/75 dark:text-slate-100/75">
+                      {data.description}
+                    </p>
+                    <Button size="sm" variant="secondary" className="mt-4">
+                      View Program
+                    </Button>
+                  </div>
+                </div>
+              </Link>
             );
           })}
         </div>
       </main>
-    </>
-  );
-}
-
-function ProgramCard(props: any) {
-  return (
-    <>
-      <Link
-        href={props.URL}
-        className="hover:opacity-80 active:ring-1 transition-all"
-      >
-        <div className="bg-primary-50 dark:bg-primary-900 min-h-[430px] rounded-xl">
-          <div className="w-full h-64 relative">
-            <Image
-              fill
-              src="/media/hero-image.webp"
-              alt={props.Headline}
-              className="object-cover rounded-xl hover:grayscale-[10%]"
-            />
-          </div>
-          <div className="p-8">
-            <h2 className="text-lg font-semibold text-slate-900 dark:text-slate-100">
-              {props.Headline}
-            </h2>
-            <p className="leading-7 line-clamp-3 tracking-wider text-opacity-75 text-slate-700/75 dark:text-slate-100/75">
-              {props.Supporting}
-            </p>
-            <Button size="sm" variant="secondary" className="mt-4">
-              View Program
-            </Button>
-          </div>
-        </div>
-      </Link>
     </>
   );
 }
