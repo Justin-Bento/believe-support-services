977f5796ce14e27c06f6061ad95a6271447a202c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-07 15:12:49 -0500

    Moved contact Form functional component to main program. To reduce complexity

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -4,6 +4,8 @@ import { Switch } from "@headlessui/react";
 import { useState } from "react";
 
 export default function contact() {
+  const [agreed, setAgreed] = useState(false);
+
   return (
     <>
       <Head>
@@ -30,7 +32,150 @@ export default function contact() {
         {/*
          * Contact Center
          */}
-        <ContactForm />
+        <form action="#" method="POST" className="">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div>
+              <label htmlFor="first-name" className="body-medium">
+                First name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md  bg-transparent dark:bg-primary-950   border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="last-name" className="body-medium">
+                Last name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autoComplete="family-name"
+                  className="bg-transparent  dark:bg-primary-950  block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="company" className="body-medium">
+                Company
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="company"
+                  id="company"
+                  autoComplete="organization"
+                  className="bg-transparent dark:bg-primary-950 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="email" className="body-medium">
+                Email
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  autoComplete="email"
+                  className="bg-transparent  dark:bg-primary-950  block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="phone-number" className="body-medium">
+                Phone number
+              </label>
+              <div className="relative mt-2.5">
+                <div className="absolute inset-y-0 left-0 flex items-center">
+                  <label htmlFor="country" className="sr-only">
+                    Country
+                  </label>
+                  <select
+                    id="country"
+                    name="country"
+                    className="h-full py-0 pl-4 text-gray-400 bg-transparent border-0 rounded-md dark:bg-primary-950 bg-none pr-9 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                  >
+                    <option>US</option>
+                    <option>CA</option>
+                    <option>EU</option>
+                  </select>
+                </div>
+                <input
+                  type="tel"
+                  name="phone-number"
+                  id="phone-number"
+                  autoComplete="tel"
+                  className="block w-full bg-transparent  dark:bg-primary-950  rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="message" className="body-medium">
+                Message
+              </label>
+              <div className="mt-2.5">
+                <textarea
+                  name="message"
+                  id="message"
+                  rows={4}
+                  className="bg-transparent  dark:bg-primary-950  block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  defaultValue={""}
+                />
+              </div>
+            </div>
+            <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
+              <div className="flex items-center h-6">
+                <Switch
+                  checked={agreed}
+                  onChange={setAgreed}
+                  className={classNames(
+                    agreed ? "bg-indigo-600" : "bg-gray-200",
+                    "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  )}
+                >
+                  <span className="sr-only">Agree to policies</span>
+                  <span
+                    aria-hidden="true"
+                    className={classNames(
+                      agreed ? "translate-x-3.5" : "translate-x-0",
+                      "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
+                    )}
+                  />
+                </Switch>
+              </div>
+              <Switch.Label className="body-small">
+                By selecting this, you agree to our{" "}
+                <a
+                  href="#"
+                  className="font-semibold text-gray-950 dark:text-yellow-600"
+                >
+                  privacy&nbsp;policy
+                </a>
+                .
+              </Switch.Label>
+            </Switch.Group>
+          </div>
+          <div className="mt-10">
+            <button
+              type="submit"
+              className="block w-full rounded-md bg-yellow-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
+            >
+              Let&#39;s talk
+            </button>
+          </div>
+        </form>
+        {/*
+         * Contact Form
+         */}
         <CompanyOffices />
       </main>
     </>
@@ -40,152 +185,6 @@ function classNames(...classes: any) {
   return classes.filter(Boolean).join(" ");
 }
 
-function ContactForm() {
-  const [agreed, setAgreed] = useState(false);
-  return (
-    <form action="#" method="POST" className="">
-      <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-        <div>
-          <label htmlFor="first-name" className="body-medium">
-            First name
-          </label>
-          <div className="mt-2.5">
-            <input
-              type="text"
-              name="first-name"
-              id="first-name"
-              autoComplete="given-name"
-              className="block w-full rounded-md  bg-transparent dark:bg-primary-950   border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-        <div>
-          <label htmlFor="last-name" className="body-medium">
-            Last name
-          </label>
-          <div className="mt-2.5">
-            <input
-              type="text"
-              name="last-name"
-              id="last-name"
-              autoComplete="family-name"
-              className="bg-transparent  dark:bg-primary-950  block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-        <div className="sm:col-span-2">
-          <label htmlFor="company" className="body-medium">
-            Company
-          </label>
-          <div className="mt-2.5">
-            <input
-              type="text"
-              name="company"
-              id="company"
-              autoComplete="organization"
-              className="bg-transparent dark:bg-primary-950 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-        <div className="sm:col-span-2">
-          <label htmlFor="email" className="body-medium">
-            Email
-          </label>
-          <div className="mt-2.5">
-            <input
-              type="email"
-              name="email"
-              id="email"
-              autoComplete="email"
-              className="bg-transparent  dark:bg-primary-950  block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-        <div className="sm:col-span-2">
-          <label htmlFor="phone-number" className="body-medium">
-            Phone number
-          </label>
-          <div className="relative mt-2.5">
-            <div className="absolute inset-y-0 left-0 flex items-center">
-              <label htmlFor="country" className="sr-only">
-                Country
-              </label>
-              <select
-                id="country"
-                name="country"
-                className="h-full py-0 pl-4 text-gray-400 bg-transparent border-0 rounded-md dark:bg-primary-950 bg-none pr-9 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-              >
-                <option>US</option>
-                <option>CA</option>
-                <option>EU</option>
-              </select>
-            </div>
-            <input
-              type="tel"
-              name="phone-number"
-              id="phone-number"
-              autoComplete="tel"
-              className="block w-full bg-transparent  dark:bg-primary-950  rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-        <div className="sm:col-span-2">
-          <label htmlFor="message" className="body-medium">
-            Message
-          </label>
-          <div className="mt-2.5">
-            <textarea
-              name="message"
-              id="message"
-              rows={4}
-              className="bg-transparent  dark:bg-primary-950  block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              defaultValue={""}
-            />
-          </div>
-        </div>
-        <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
-          <div className="flex items-center h-6">
-            <Switch
-              checked={agreed}
-              onChange={setAgreed}
-              className={classNames(
-                agreed ? "bg-indigo-600" : "bg-gray-200",
-                "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              )}
-            >
-              <span className="sr-only">Agree to policies</span>
-              <span
-                aria-hidden="true"
-                className={classNames(
-                  agreed ? "translate-x-3.5" : "translate-x-0",
-                  "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
-                )}
-              />
-            </Switch>
-          </div>
-          <Switch.Label className="body-small">
-            By selecting this, you agree to our{" "}
-            <a
-              href="#"
-              className="font-semibold text-gray-950 dark:text-yellow-600"
-            >
-              privacy&nbsp;policy
-            </a>
-            .
-          </Switch.Label>
-        </Switch.Group>
-      </div>
-      <div className="mt-10">
-        <button
-          type="submit"
-          className="block w-full rounded-md bg-yellow-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
-        >
-          Let&#39;s talk
-        </button>
-      </div>
-    </form>
-  );
-}
 function CompanyOffices() {
   return (
     <div className="">
