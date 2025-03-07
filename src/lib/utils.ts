460542f787535099c8c84af57c98e432254eefcc
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-07 15:50:56 -0500

    Moved classNames to utils pages.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,7 @@
 export function CreateSlug(slug: string) {
   return slug.toString().replace(/\s+/g, "-").toLowerCase()
 }
+// Utility function for conditional class names
+export function classNames(...classes: (string | boolean)[]): string {
+  return classes.filter(Boolean).join(" ");
+}
