4afe07d699c5ea48880009f351b325b57f289c9b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-08 13:10:12 -0500

    Changed event type to support light mode.

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -74,7 +74,7 @@ export default function events() {
                 <li className="col-span-8 p-6 space-y-4">
                   <button
                     type="button"
-                    className="rounded bg-primary-950/50 px-3 py-1.5 text-xs font-mono uppercase text-primary-50 dark:hover:bg-primary-700"
+                    className="rounded bg-primary-400 dark:bg-primary-950/50 px-3 py-1.5 text-xs font-mono uppercase text-primary-50 dark:hover:bg-primary-700"
                   >
                     Event Type
                   </button>
