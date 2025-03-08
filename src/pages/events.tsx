f96f9997732ae49a8ff7bee1c0e38f2cdc9aa1c3
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-08 11:32:01 -0500

    Created an unorrederd list of events people can filter through belieev support services.

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -10,7 +10,7 @@ export default function events() {
       <main className="min-h-screen xl:container xl:mx-auto py-24">
         <section className="space-y-4">
           <h1 className="display-medium font-bold capitalize">
-            Join Us for Upcoming Events
+            Raise Awareness With Our Events
           </h1>
           <p className="text-lg max-w-[120ch] text-balance">
             Stay connected with Believe Support Services through our exciting
@@ -18,8 +18,41 @@ export default function events() {
             gatherings and celebrations, our events are designed to promote
             learning, social interaction, and a sense of belonging.
           </p>
+          <button
+            type="button"
+            className="rounded bg-primary-800 px-3 py-1.5 text-xs font-mono uppercase text-primary-50 dark:hover:bg-primary-700"
+          >
+            Events
+          </button>
         </section>
-        <section className="mt-12"></section>
+        <section className="mt-12">
+          <ul className="flex items-center gap-4">
+            <li className="">
+              <button
+                type="button"
+                className="rounded bg-transparent ring-2 px-3 py-1.5 text-xs font-mono uppercase text-primary-50 dark:hover:bg-primary-700"
+              >
+                All Events
+              </button>
+            </li>
+            <li className="">
+              <button
+                type="button"
+                className="rounded bg-transparent ring-2 px-3 py-1.5 text-xs font-mono uppercase text-primary-50 dark:hover:bg-primary-700"
+              >
+                Upcomming Events
+              </button>
+            </li>
+            <li className="">
+              <button
+                type="button"
+                className="rounded bg-transparent ring-2 px-3 py-1.5 text-xs font-mono uppercase text-primary-50 dark:hover:bg-primary-700"
+              >
+                Previous Events
+              </button>
+            </li>
+          </ul>
+        </section>
       </main>
     </>
   );
