7cd785302a4715674cb2e3a8260e921f02189e4a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-25 00:46:45 -0400

    Created a bottom navigation.

---
+++ src/app/components/NavigationBottom.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NavigationBottom() {
+  return (
+    <footer className="bg-gray-800 p-4">
+      <div className="container mx-auto text-white">
+        <p>&copy; 2023 My App. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
