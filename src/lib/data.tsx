c3526977d6c91329ffe22d7c5ed680413e5c18ec
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-25 17:22:29 -0400

    Removed type ambiguity

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -2,6 +2,12 @@ interface NavigationTopLinks {
   name: string;
   href: string;
 }
+interface SocialLink {
+  name: string;
+  href: string;
+  icon: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+
 const NavigationTopLinks: NavigationTopLinks[] = [
   { name: "Home", href: "/" },
   { name: "About", href: "/about" },
@@ -134,7 +140,7 @@ const NavigationBottomLinks = {
     {
       name: "Facebook",
       href: "#",
-      icon: (props: any) => (
+      icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
@@ -147,7 +153,7 @@ const NavigationBottomLinks = {
     {
       name: "Instagram",
       href: "#",
-      icon: (props: any) => (
+      icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
@@ -160,7 +166,7 @@ const NavigationBottomLinks = {
     {
       name: "X",
       href: "#",
-      icon: (props: any) => (
+      icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
         </svg>
@@ -169,7 +175,7 @@ const NavigationBottomLinks = {
     {
       name: "GitHub",
       href: "#",
-      icon: (props: any) => (
+      icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
@@ -182,7 +188,7 @@ const NavigationBottomLinks = {
     {
       name: "YouTube",
       href: "#",
-      icon: (props: any) => (
+      icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
