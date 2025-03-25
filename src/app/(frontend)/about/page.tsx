5e8ccb3c11455c54a772a26a49e5b45b3d224925
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-25 14:27:51 -0400

    Created about page.

---
+++ src/app/(frontend)/about/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <main className="container mx-auto min-h-dvh my-24 space-y-32">
+      <h1 className="">About Page</h1>
+    </main>
+  );
+}
