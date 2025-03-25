51bbabd202c6becc81df074d8779a77701404145
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-25 00:40:01 -0400

    deleted nextjs14

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-export function CreateSlug(slug: string) {
-  return slug.toString().replace(/\s+/g, "-").toLowerCase()
-}
-// Utility function for conditional class names
-export function classNames(...classes: (string | boolean)[]): string {
-  return classes.filter(Boolean).join(" ");
-}
