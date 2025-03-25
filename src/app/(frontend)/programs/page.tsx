bb8a122384e3d192bf3a5be4455a6a7b184bbfdd
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-25 14:51:20 -0400

    Updated id

--- src/app/(frontend)/programs/page.tsx
+++ src/app/(frontend)/programs/page.tsx
@@ -6,6 +6,7 @@ import { programs_info } from "@/lib/data";
 import { CreateSlug } from "@/lib/utils";
 
 interface programs_info_interface {
+  id: number;
   title: string;
   description: string;
 }
@@ -30,40 +31,38 @@ export default function page() {
       <section className="grid sm:grid-cols-2 2xl:grid-cols-3 gap-8 mt-12">
         {programs_info.map((data: programs_info_interface) => {
           return (
-            <>
-              <Link
-                key={data.title}
-                href={`/programs/${CreateSlug(data.title)}`}
-                className={`group transition-all rounded-xl border-2 border-primary-100 active:bg-primary-100 dark:border-primary-800 bg-primary-50 hover:border-primary-200 hover:bg-primary-50 dark:bg-primary-900 dark:hover:bg-primary-800 dark:active:bg-primary-700 ${
-                  hoveredCard && hoveredCard !== data.title ? "opacity-50" : ""
-                }`}
-                onMouseEnter={() => setHoveredCard(data.title)}
-                onMouseLeave={() => setHoveredCard(null)}
-              >
-                <div className="w-full h-64 relative overflow-hidden">
-                  <Image
-                    fill
-                    src="/media/hero-image.webp"
-                    alt={data.title}
-                    className="object-cover rounded-t-xl group-hover:contrast-125"
-                  />
-                </div>
-                <div className="p-6 space-y-4">
-                  <p className="text-lg font-bold leading-none tracking-tight">
-                    {data.title}
-                  </p>
-                  <p className="text-sm leading-5 opacity-80 line-clamp-3">
-                    {data.description}
-                  </p>
-                  <button
-                    type="button"
-                    className="rounded-md bg-transparent pr-2.5 py-1 text-xs font-medium capitalize text-gray-900 group-hover:underline dark:text-white dark:opacity-70"
-                  >
-                    Read About The Program &rarr;
-                  </button>
-                </div>
-              </Link>
-            </>
+            <Link
+              key={data.id}
+              href={`/programs/${CreateSlug(data.title)}`}
+              className={`group transition-all rounded-xl border-2 border-primary-100 active:bg-primary-100 dark:border-primary-800 bg-primary-50 hover:border-primary-200 hover:bg-primary-50 dark:bg-primary-900 dark:hover:bg-primary-800 dark:active:bg-primary-700 ${
+                hoveredCard && hoveredCard !== data.title ? "opacity-50" : ""
+              }`}
+              onMouseEnter={() => setHoveredCard(data.title)}
+              onMouseLeave={() => setHoveredCard(null)}
+            >
+              <div className="w-full h-64 relative overflow-hidden">
+                <Image
+                  fill
+                  src="/media/hero-image.webp"
+                  alt={data.title}
+                  className="object-cover rounded-t-xl group-hover:contrast-125"
+                />
+              </div>
+              <div className="p-6 space-y-4">
+                <p className="text-lg font-bold leading-none tracking-tight">
+                  {data.title}
+                </p>
+                <p className="text-sm leading-5 opacity-80 line-clamp-3">
+                  {data.description}
+                </p>
+                <button
+                  type="button"
+                  className="rounded-md bg-transparent pr-2.5 py-1 text-xs font-medium capitalize text-gray-900 group-hover:underline dark:text-white dark:opacity-70"
+                >
+                  Read About The Program &rarr;
+                </button>
+              </div>
+            </Link>
           );
         })}
       </section>
--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -51,31 +51,37 @@ const CompanyValues = [
 */
 export const programs_info = [
   {
+    id: 1,
     title: "Recreation and Leisure",
     description:
       "Our Recreation and Leisure programs offer a range of engaging activities that promote physical fitness, creative expression, and social interaction. Participants can explore new interests, make friends, and experience the joy of shared experiences.",
   },
   {
+    id: 2,
     title: "Group Living",
     description:
       "From effective communication and problem-solving to time management and personal care, our comprehensive curriculum equips participants with the skills necessary to navigate life's challenges.",
   },
   {
+    id: 3,
     title: "Residential Program",
     description:
       "In the digital age, having strong technology skills is crucial. Our Technology and Digital Literacy program helps individuals navigate the digital world with confidence. Participants learn essential computer skills, online safety practices, and how to utilize technology to enhance their communication, learning, and daily activities.",
   },
   {
+    id: 4,
     title: "Respite Support",
     description:
       "Wellness and Mindfulness programs focus on promoting mental and emotional well-being, providing participants with tools to manage stress, anxiety, and emotional challenges. Through meditation, mindfulness practices, and psychoeducation, individuals can develop resilience and cultivate a positive sense of self.",
   },
   {
+    id: 5,
     title: "Vocational Camp",
     description:
       "Our Vocational Training and Employment program is designed to equip individuals with the skills and confidence needed to enter the workforce with pride and purpose. Through a combination of skill-building workshops, career exploration, and hands-on training, participants can develop valuable vocational skills that lead to meaningful employment opportunities.",
   },
   {
+    id: 6,
     title: "Social Group Engagement",
     description:
       "Building meaningful connections is at the core of our Social Group Engagement programs. We provide inclusive spaces where individuals can connect with peers who share similar interests and experiences. Through group discussions, collaborative activities, and shared experiences, participants can develop lasting friendships and strengthen their social skills..",
