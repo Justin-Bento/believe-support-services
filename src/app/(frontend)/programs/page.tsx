9f8360395c8973fbd2a57141823c98c5a662fd66
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-29 23:31:15 -0400

    Added a new div container to represent an empty image.

--- src/app/(frontend)/programs/page.tsx
+++ src/app/(frontend)/programs/page.tsx
@@ -18,7 +18,9 @@ export default function ProgramsPage() {
        */}
       <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         <div className="overflow-hidden rounded-lg bg-gray-200">
-          <div className="px-4 py-5 sm:p-6">{/* Content goes here */}</div>
+          <div className="px-4 py-5 sm:p-6">
+            <div className="w-full h-48 bg-foreground rounded-2xl"></div>
+          </div>
         </div>
       </section>
       {/*
