0405bf24d54d084c8919a890a02728704a7671b0
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-29 23:56:42 -0400

    Grabbed data from program info object and injected content to the custom card component.

--- src/app/(frontend)/programs/page.tsx
+++ src/app/(frontend)/programs/page.tsx
@@ -1,3 +1,4 @@
+import { programs_info } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -19,33 +20,37 @@ export default function ProgramsPage() {
        * End of Page Header
        */}
       <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 mt-10">
-        <div className="relative group isolate overflow-hidden rounded-lg bg-primary/15 hover:bg-primary/20 transition-all">
-          <div className="px-4 py-5 sm:p-6 flex items-start flex-col gap-3">
-            <div className="w-full h-48 relative">
-              <Image
-                fill
-                src="/media/believe-day-program.webp"
-                alt=""
-                className="object-cover object-top rounded-xl"
-              />
+        {programs_info.map((info, index) => {
+          return (
+            <div
+              key={index}
+              className="relative group isolate overflow-hidden rounded-lg bg-primary/15 hover:bg-primary/20 transition-all"
+            >
+              <div className="px-4 py-5 sm:p-6 flex items-start flex-col gap-3">
+                <div className="w-full h-48 relative">
+                  <Image
+                    fill
+                    src="/media/hero-image.webp"
+                    alt=""
+                    className="object-cover object-top rounded-xl"
+                  />
+                </div>
+                <h2 className="capitalize text-xl font-bold tracking-[0.01rem]">
+                  <Link href="#" className=" group-hover:underline">
+                    {info.title}
+                    <span className="absolute inset-0"></span>
+                  </Link>
+                </h2>
+                <p className="line-clamp-4 text-base/[1.25] tracking-wide">
+                  {info.description}.
+                </p>
+                <button className="rounded-sm block py-1 text-xs font-semibold text-sky-800 ">
+                  View Program Information &rarr;
+                </button>
+              </div>
             </div>
-            <h2 className="capitalize text-xl font-bold tracking-[0.01rem]">
-              <Link href="#" className=" group-hover:underline">
-                The Believe Day Program
-                <span className="absolute inset-0"></span>
-              </Link>
-            </h2>
-            <p className="line-clamp-4 text-base/[1.25] tracking-wide">
-              This is for individuals with complex needs, providing programming
-              that has been developed to empower and encourage self-growth. If
-              you are looking for a fun way to learn and create new memories
-              with old and new friends, this program is for you.
-            </p>
-            <button className="rounded-sm block py-1 text-xs font-semibold text-sky-800 ">
-              View Program Information &rarr;
-            </button>
-          </div>
-        </div>
+          );
+        })}
       </section>
       {/*
        * End of Company Programs
